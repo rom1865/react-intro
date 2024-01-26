8d9064d5f40ed06fc680d23b40b9674a40164ac5
crev0004 <romain.crevet@etudiant.univ-reims.fr>
2024-01-26 14:10:35 +0100

    tp 1 Q5 Un premier composant

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "./components/Card.jsx";
 
 function App() {
   return (
@@ -6,7 +7,14 @@ function App() {
       <header className="app__header header">
         <h1 className="header__title">Introduction to React</h1>
       </header>
-      <main className="app__main">main</main>
+      <main className="app__main">
+        <div className="cards">
+          <Card title="title 1" />
+          <Card title="title 2" />
+          <Card title="title 3" />
+          <Card title="title 4" />
+        </div>
+      </main>
       <footer className="app__footer footer">footer</footer>
     </div>
   );

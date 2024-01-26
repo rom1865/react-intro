c550502c4adfe1fa6cea697b774990b11e877f72
crev0004 <romain.crevet@etudiant.univ-reims.fr>
2024-01-26 14:33:46 +0100

    tp 1 Q7 Décomposition du paramètre de propriétés

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 function Card(props) {
+  const { title } = props;
   return (
     <article className="card">
-      <header className="card__header header__title">{props.title}</header>
+      <header className="card__header header__title">{title}</header>
       <section className="card__main">Content</section>
     </article>
   );
